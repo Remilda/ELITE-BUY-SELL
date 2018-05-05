4e6bab45c9ab30626d9bc2a064162ca2d6250dcb
abhijeet9920 <abhijeet.umbarkarcmpn2@gmail.com>
2018-05-06 00:58:25 +0530

    Add auction

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.json());
 app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads/products'));
+app.use(express.static(__dirname + '/uploads/auctions'));
 
 app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 app.use(flash());
--- models/Auction.js
+++ models/Auction.js
@@ -5,8 +5,6 @@ var User = mongoose.model('User');
 
 var AuctionSchema = new mongoose.Schema({
 	title: {type:String, require: [true, "Title is required"]},
-	is_premium: {type:Boolean, default: false},
-	maximum_products: Number,
 	location: {type:String, require:[true, "Location is required"]},
 	start_date: {type:Date, require: [true, "Mention when will be aution starts"]},
 	end_date: Date,
@@ -15,6 +13,10 @@ var AuctionSchema = new mongoose.Schema({
 	    ref: 'User',
 	    default:null
 	},
+	minimum_price:{type:Number, require:[true, "Price must be given"]},
+	description: {type:String, require:[true, "Description is required"]},
+	img_name: {type:String},
+	img_path: {type:String}
 }, {timestamps: true});
 
 
